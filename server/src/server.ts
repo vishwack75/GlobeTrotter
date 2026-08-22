1fb391d833729f9776be86677cce1a63805cfe70
vishwack75 <vishwachakrani0140@gmail.com>
2026-08-22 10:08:32 +0530

    feat : initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running 🚀"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
